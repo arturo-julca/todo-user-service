86e20b11def37aa235386991c5fce1aaa1a7ff1c
ArturoJulca-minted <arturo.julca@minted.com>
2019-03-12 22:20:35 -0500

    First commit

---
+++ src/main/java/com/avantica/todouserservice/TodoUserServiceApplication.java
@@ -0,0 +1,13 @@
+package com.avantica.todouserservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TodoUserServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TodoUserServiceApplication.class, args);
+	}
+
+}
