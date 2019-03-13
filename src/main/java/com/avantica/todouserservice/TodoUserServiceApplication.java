d457bca24aa72431c3e2e366ff1df6d7c690d015
ArturoJulca-minted <arturo.julca@minted.com>
2019-03-13 01:47:20 -0500

    Added Eureka client configuration

--- src/main/java/com/avantica/todouserservice/TodoUserServiceApplication.java
+++ src/main/java/com/avantica/todouserservice/TodoUserServiceApplication.java
@@ -5,12 +5,14 @@ import java.util.stream.LongStream;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 import org.springframework.context.annotation.Bean;
 
 import com.avantica.todouserservice.model.AppUser;
 import com.avantica.todouserservice.repository.AppUserRepository;
 
 @SpringBootApplication
+@EnableEurekaClient 	// It acts as a eureka client
 public class TodoUserServiceApplication {
 
 	public static void main(String[] args) {
